021f01e1c9c07ff4ad69e56ee276e31407ce79aa
harshil <harshil4076@gmail.com>
2023-04-12 08:24:03 -0400

    added react-speech-recognition

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { V2_MetaFunction } from "@remix-run/react";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
@@ -7,11 +8,30 @@ export const meta: V2_MetaFunction = () => {
 
 
 export default function Index() {
-  const startDictation = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
   }
+  const startListening = () => {
+    SpeechRecognition.startListening({ continuous: true });
+  };
+  const stopListening = () => {
+    SpeechRecognition.stopListening();
+  };
+
   return (
     <>
     <div className="text-4xl text-center">Welcome to Speech to Text</div>
-    <input type="button" value="Ask.." onClick={startDictation} />
+    <p>Microphone: {listening ? 'on' : 'off'}</p>
+     <button onClick={startListening}>Start</button>
+      <button onClick={stopListening}>Stop</button>
+       <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
     </>);
 }
